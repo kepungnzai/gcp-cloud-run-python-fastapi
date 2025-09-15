32e79670cb9b55e37adf65c0ed4f3bb7eac8ce1c
jeremy woo <kepungnzai@gmail.com>
2025-09-16 04:35:03 +1200

    update code to decode pub/sub message

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+import base64
+import datetime
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+@app.post("/")
+async def pubsub_push(request: Request):
+    
+    envelope = await request.json()
 
-@app.get("/")
+    if not envelope or "message" not in envelope:
+        return JSONResponse(status_code=400, content={"error": "Bad Request"})
 
-def hello(name: str = "World"):
-   """Return a friendly HTTP greeting."""
-   return {
-      "message": f"Hello {name}!"
-   }
+    pubsub_message = envelope["message"]
+
+    data = ""
+    if "data" in pubsub_message:
+        data = base64.b64decode(pubsub_message["data"]).decode("utf-8")
+
+    print(f"Received Pub/Sub message: {data}")  
+    
+    return JSONResponse(status_code=200, content=data)
